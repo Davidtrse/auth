8d56385ab141127773a20edd7fd241bdab89d233
Your Name <you@example.com>
2022-03-23 22:19:45 +0700

    rotating keys example

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha512"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	uuid "github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -100,9 +103,60 @@ func checkSig(msg, sig []byte) (bool, error) {
 
 func createToken(c *UserClaims) (string, error) {
 	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	signedToken, err := t.SignedString(key)
+	signedToken, err := t.SignedString(keys[currentKid])
 	if err != nil {
 		return "", fmt.Errorf("error in createToken when signing token: %w", err)
 	}
 	return signedToken, nil
+}
+
+func generateNewKey() error {
+	newKey := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, newKey)
+	if err != nil {
+		return fmt.Errorf("Error in generateNewKey while generating key: %w", err)
+	}
+	uid := uuid.NewV4()
+	keys[uid.String()] = Key{
+		key:     newKey,
+		created: time.Now(),
+	}
+	return nil
+}
+
+type Key struct {
+	key     []byte
+	created time.Time
+}
+
+var currentKid = ""
+var keys = map[string]Key{}
+
+func parseToken(signedToken string) (*UserClaims, error) {
+	claims := &UserClaims{}
+	t, err := jwt.ParseWithClaims(signedToken, claims, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() == jwt.SigningMethodES512.Alg() {
+			return nil, fmt.Errorf("Invalid signing algorithum")
+		}
+
+		kid, ok := t.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("Invalid key ID")
+		}
+		k, ok := keys[kid]
+		if !ok {
+			return nil, fmt.Errorf("invalid key ID")
+		}
+
+		return k.key, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error in pasrse Token while parsing token: %w", err)
+	}
+	if !t.Valid {
+		return nil, fmt.Errorf("Error in parse Toekn, token is not valid")
+	}
+	claims = t.Claims.(*UserClaims)
+	return claims, nil
 }
